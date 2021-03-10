7bc203f53ed95a5681108242be0be59f69ffe1e6
oshinoshin <donateluro@gmail.com>
2021-03-10 16:55:07 +0900

    データベースにレビューを保存する機能の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,12 +9,18 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book =Book.new(book_params)
-    if @prototype.save
+    @book = Book.new(book_params)
+    if @book.save
       redirect_to root_path
     else
       render :new
     end
   end
-  
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :image, :review).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,9 +2,11 @@ class Book < ApplicationRecord
  
   with_options presence: true do
     validates :title
-    validates :concept
+    validates :review
   end
 
   has_many   :comments
   belongs_to :user
+  has_one_attached :image
+  
 end

dbba718359f8ce6b0a0c65302836e47f955bfa70
oshinoshin <donateluro@gmail.com>
2021-03-25 13:07:13 +0900

    ユーザーに紐づいたbookのみ取得

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @name = @user.name
-    @books = @user.books
-    @books = Book.page(params[:page]).per(4).order("created_at DESC")
+    @books = @user.books.page(params[:page]).per(4).order("created_at DESC")
   end
 end

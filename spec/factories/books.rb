34fd4ae9e9d324fc8ffb038d2447efbf1f2eeaf0
oshinoshin <donateluro@gmail.com>
2021-04-15 11:11:51 +0900

    正常系のテストコードの実装

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :book do
-    
+    title  {'お金の大学'}
+    review {Faker::Lorem.sentence}
+    association :user
+
+    after(:build) do |book|
+      book.image.attach(io: File.open('public/images/output-image.png'), filename: 'output-image.png')
+    end
   end
 end
+
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @book = FactoryBot.build(:book)
+end
+
+  describe 'レビューの新規投稿' do
+    context '新規登録できるとき' do
+      it 'titleとreviewとimageが存在すれば登録できる' do
+        expect(@book).to be_valid
+      end
+    end
+  
+  
+  
+  
+  end
 end

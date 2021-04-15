222512db43dd2f6125cfc8e7248715c5611609a1
oshinoshin <donateluro@gmail.com>
2021-04-15 11:30:16 +0900

    異常系のテストコードの実装とエラーメッセージの日本語化

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,17 +3,35 @@ require 'rails_helper'
 RSpec.describe Book, type: :model do
   before do
     @book = FactoryBot.build(:book)
-end
+  end
 
-  describe 'レビューの新規投稿' do
+  describe 'レビューの投稿ができるとき' do
     context '新規登録できるとき' do
       it 'titleとreviewとimageが存在すれば登録できる' do
         expect(@book).to be_valid
       end
     end
   
-  
-  
-  
+    context 'レビューの投稿ができないとき' do
+      it 'imageが空では登録できない' do
+        @book.image = nil
+        @book.valid?
+        expect(@book.errors.full_messages).to include("Imageを入力してください")
+      end
+
+      it 'titleが空では登録できない' do
+        @book.title = nil
+        @book.valid?
+        expect(@book.errors.full_messages).to include("Titleを入力してください")
+      end
+
+      it 'reviewが空では登録できない' do
+        @book.review = nil
+        @book.valid?
+        expect(@book.errors.full_messages).to include("Reviewを入力してください")
+      end
+
+
+    end
   end
 end

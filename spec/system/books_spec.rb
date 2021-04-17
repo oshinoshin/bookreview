8547529317ee91637ce67dff30635b3d28f70cc8
oshinoshin <donateluro@gmail.com>
2021-04-17 10:40:11 +0900

    レビューの編集ページ遷移までのテストコード実装

--- spec/system/books_spec.rb
+++ spec/system/books_spec.rb
@@ -47,3 +47,42 @@ RSpec.describe "レビュー投稿", type: :system do
   end
 end
  
+RSpec.describe 'レビュー編集', type: :system do
+  before do
+    @book1 = FactoryBot.create(:book)
+    @book2 = FactoryBot.create(:book)
+  end
+  context 'レビュー編集ができるとき' do
+    it 'ログインしたユーザーは自分が投稿したツイートの編集ができる' do
+      # レビュー1を投稿したユーザーでログインする
+      visit new_user_session_path
+      fill_in 'Eメール', with: @book1.user.email
+      fill_in 'user_password', with: @book1.user.password
+      find('input[name="commit"]').click
+      expect(current_path).to eq(root_path)
+      # レビュー1に「編集」ボタンがあることを確認する
+
+      # 編集ページへ遷移する
+      visit edit_book_path(@book1)
+      # すでに投稿済みの内容がフォームに入っていることを確認する
+      # 投稿内容を編集する
+      # 編集してもTweetモデルのカウントは変わらないことを確認する
+      # 編集完了画面に遷移したことを確認する
+      # 「更新が完了しました」の文字があることを確認する
+      # トップページに遷移する
+      # トップページには先ほど変更した内容のツイートが存在することを確認する（画像）
+      # トップページには先ほど変更した内容のツイートが存在することを確認する（テキスト）
+    end
+  end
+  context 'ツイート編集ができないとき' do
+    it 'ログインしたユーザーは自分以外が投稿したツイートの編集画面には遷移できない' do
+      # ツイート1を投稿したユーザーでログインする
+      # ツイート2に「編集」ボタンがないことを確認する
+    end
+    it 'ログインしていないとツイートの編集画面には遷移できない' do
+      # トップページにいる
+      # ツイート1に「編集」ボタンがないことを確認する
+      # ツイート2に「編集」ボタンがないことを確認する
+    end
+  end
+end
